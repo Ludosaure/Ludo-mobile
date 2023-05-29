ccf1a344fb840e59e773e6a7e1e28405c83a80db
amelie-qqch <48447617+amelie-qqch@users.noreply.github.com>
2023-05-29 19:43:12 +0200

    fix/responsive: appbar icon

--- lib/ui/components/nav_bar/app_bar/custom_app_bar.dart
+++ lib/ui/components/nav_bar/app_bar/custom_app_bar.dart
@@ -1,21 +1,40 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:ludo_mobile/utils/app_constants.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({Key? key}) : super(key: key);
 
   @override
   PreferredSizeWidget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return AppBar(
       backgroundColor: Colors.white,
-      toolbarHeight: MediaQuery.of(context).size.height * 0.075,
+      toolbarHeight: screenSize.height * 0.075,
       title: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Image(
           image: const AssetImage(AppConstants.APP_LOGO),
-          width: MediaQuery.of(context).size.width * 0.15,
-          height: MediaQuery.of(context).size.width * 0.15,
+          width: ResponsiveValue(
+            context,
+            defaultValue: 50.0,
+            valueWhen: [
+              const Condition.largerThan(name: MOBILE, value: 75.0),
+              const Condition.largerThan(name: TABLET, value: 75.0),
+              const Condition.largerThan(name: DESKTOP, value: 80.0),
+            ],
+          ).value,
+          height: ResponsiveValue(
+            context,
+            defaultValue: 50.0,
+            valueWhen: [
+              const Condition.largerThan(name: MOBILE, value: 75.0),
+              const Condition.largerThan(name: TABLET, value: 75.0),
+              const Condition.largerThan(name: DESKTOP, value: 80.0),
+            ],
+          ).value,
+          fit: BoxFit.contain,
         ),
       ),
       centerTitle: true,
@@ -33,10 +52,10 @@ class CustomAppBar extends StatelessWidget {
           ).tr(),
         ),
       ),
-      leadingWidth: MediaQuery.of(context).size.width * 0.35,
+      leadingWidth: screenSize.width * 0.35,
       actions: [
         SizedBox(
-          width: MediaQuery.of(context).size.width * 0.35,
+          width: screenSize.width * 0.35,
           child: Center(
               child: RichText(
             textAlign: TextAlign.center,
