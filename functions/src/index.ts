3f8d9951f479cd36637a0d1febbeea4101741174
ame <amedouillard@hotmail.fr>
2023-07-03 15:40:50 +0200

    divers fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,28 +9,28 @@ export const pushNotificationForNewMessage = functions.firestore.document('conve
     .onUpdate(async snapshot => {
         const database = admin.firestore();
         const messaging = admin.messaging();
-        const message = snapshot.after.data();
+        const conversation = snapshot.after.data();
 
         const sender = await database
             .collection('users')
-            .doc(message.recentMessageSender)
+            .doc(conversation.recentMessageSender)
             .get();
 
         const payload = {
             notification: {
-                title: 'Nouveau message de : ' + sender.data()!.firstname,
-                body: message.recentMessage,
+                title: 'Nouveau message de ' + sender.data()!.firstname,
+                body: conversation.recentMessage,
                 icon: 'https://www.laludosaure.fr/Files/136017/Img/14/ludosaure-logo-min.png'
             }
         }
 
-        message.members.forEach(async (memberId: string) => {
+        conversation.members.forEach(async (memberId: string) => {
             const receiver = await database
                 .collection('users')
                 .doc(memberId)
                 .get();
 
-            if(receiver.data()!.id !== message.recentMessageSender) {
+            if(receiver.data()!.uid !== sender.data()!.uid) {
                 const token = receiver.data()!.token;
                 messaging.sendToDevice(token, payload);
             }
