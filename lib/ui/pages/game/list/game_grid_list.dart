5add03677c59d6c6e9775be7207d2e1f2502cb6a
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-07-05 00:03:20 +0200

    [AVAILABLE GAMES]

--- lib/ui/pages/game/list/game_grid_list.dart
+++ lib/ui/pages/game/list/game_grid_list.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ludo_mobile/domain/models/game.dart';
+import 'package:ludo_mobile/ui/components/game_filter_tab_bar.dart';
 import 'package:ludo_mobile/ui/pages/game/list/game_card.dart';
 import 'package:ludo_mobile/ui/router/routes.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
@@ -15,6 +17,34 @@ class GameGridList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _buildTabBar(context);
+  }
+
+  Widget _buildTabBar(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          const GameFilterTabBar(),
+          Expanded(
+            child: TabBarView(
+              children: [
+                _buildGamesList(context, games),
+                _buildAvailableGamesList(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvailableGamesList(BuildContext context) {
+    final availableGames = games.where((game) => game.isAvailable!).toList();
+    return _buildGamesList(context, availableGames);
+  }
+
+  Widget _buildGamesList(BuildContext context, List<Game> games) {
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: _getItemNb(context),
